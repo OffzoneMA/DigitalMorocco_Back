07936ac5cef834fd1ec2d99516e0de21ee58ad27
ichane31 <roukeyaassouma@gmail.com>
2024-05-05 16:17:05 +0200

    blogs

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const app = express();
 app.use(cors());
 
 
-app.use(express.json({limit: "100mb", extended: true, parameterLimit: 100000}));
+app.use(express.json({limit: "100mb", extended: true}));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL ,{ useNewUrlParser: true, useUnifiedTopology: true })
